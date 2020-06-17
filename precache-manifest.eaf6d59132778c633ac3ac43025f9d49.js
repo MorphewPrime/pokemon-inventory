314f4830aa8fed1b67aff57afcb32ad0ced3a967
jack Morphew <jackmorphew101@gmail.com>
2020-06-17 14:16:30 -0500

    Updates

---
+++ precache-manifest.eaf6d59132778c633ac3ac43025f9d49.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdc18bde3f56c36d5b59b4a05f137e1a",
+    "url": "/pokemon-inventory/index.html"
+  },
+  {
+    "revision": "38bc1423abc4ddc94cd2",
+    "url": "/pokemon-inventory/static/css/main.dfafcf0e.chunk.css"
+  },
+  {
+    "revision": "232b627f4777a7f9702b",
+    "url": "/pokemon-inventory/static/js/2.cbb3957f.chunk.js"
+  },
+  {
+    "revision": "38bc1423abc4ddc94cd2",
+    "url": "/pokemon-inventory/static/js/main.f41b19a2.chunk.js"
+  },
+  {
+    "revision": "a2d9e94adb82bab75d5b",
+    "url": "/pokemon-inventory/static/js/runtime~main.472934c4.js"
+  },
+  {
+    "revision": "ad51855324bf3d004caef993ea6fc181",
+    "url": "/pokemon-inventory/static/media/Bug.ad518553.png"
+  },
+  {
+    "revision": "3e8245de3bc8a60f029c504593aca76a",
+    "url": "/pokemon-inventory/static/media/Dark.3e8245de.png"
+  },
+  {
+    "revision": "46fed2ab1aedfdbf18094e03a2f825e9",
+    "url": "/pokemon-inventory/static/media/Dragon.46fed2ab.png"
+  },
+  {
+    "revision": "bbbd912c24bc32516cc52ae74c119631",
+    "url": "/pokemon-inventory/static/media/Electric-backup.bbbd912c.png"
+  },
+  {
+    "revision": "1b023faae3add8a3f895d57cdc209fe6",
+    "url": "/pokemon-inventory/static/media/Electric.1b023faa.png"
+  },
+  {
+    "revision": "d230a1471294214b5696172f160a562b",
+    "url": "/pokemon-inventory/static/media/Fairy.d230a147.png"
+  },
+  {
+    "revision": "6548aa3c8280752611906be20b5c5faf",
+    "url": "/pokemon-inventory/static/media/Fighting.6548aa3c.png"
+  },
+  {
+    "revision": "9ac780f9246b7626f83bf055cf69ebcf",
+    "url": "/pokemon-inventory/static/media/Fire.9ac780f9.png"
+  },
+  {
+    "revision": "c078952a0822d19921b4618fbbef9a78",
+    "url": "/pokemon-inventory/static/media/Flying.c078952a.png"
+  },
+  {
+    "revision": "8562bf61e8c55807d41d87c9d2a2e433",
+    "url": "/pokemon-inventory/static/media/Ghost.8562bf61.png"
+  },
+  {
+    "revision": "b4e29f3bb66b424252dc0e6dd6b50720",
+    "url": "/pokemon-inventory/static/media/Grass.b4e29f3b.png"
+  },
+  {
+    "revision": "80c0bd0a7423250d6e42cde8a8c02f03",
+    "url": "/pokemon-inventory/static/media/Ground.80c0bd0a.png"
+  },
+  {
+    "revision": "ae3d5bb08dee07fb9cc113a36b2e98c9",
+    "url": "/pokemon-inventory/static/media/Ice.ae3d5bb0.png"
+  },
+  {
+    "revision": "25e223d26c56c5d9a99ef3b7f90038c2",
+    "url": "/pokemon-inventory/static/media/Normal.25e223d2.png"
+  },
+  {
+    "revision": "b9e39cbf93c45691dc2aacd88bbced61",
+    "url": "/pokemon-inventory/static/media/Poison.b9e39cbf.png"
+  },
+  {
+    "revision": "4b8cce672fb70335c410479d855b574b",
+    "url": "/pokemon-inventory/static/media/Psychic.4b8cce67.png"
+  },
+  {
+    "revision": "9ebd3c0d64000abb13f61aa2d1c96fc1",
+    "url": "/pokemon-inventory/static/media/Rock.9ebd3c0d.png"
+  },
+  {
+    "revision": "360a319b09c67967cbf401e8dc9304e7",
+    "url": "/pokemon-inventory/static/media/Steel.360a319b.png"
+  },
+  {
+    "revision": "9908a654d610b0ccc1594d0ba890a46c",
+    "url": "/pokemon-inventory/static/media/Water.9908a654.png"
+  },
+  {
+    "revision": "9dd7e05ec2abaf6df0fd72ab4137767e",
+    "url": "/pokemon-inventory/static/media/candy.9dd7e05e.png"
+  },
+  {
+    "revision": "c8fc1f7e1d6d51fdf4fadcde990b1a8c",
+    "url": "/pokemon-inventory/static/media/external.c8fc1f7e.png"
+  },
+  {
+    "revision": "f7bd3fd99921d8d5ff073eabb60db872",
+    "url": "/pokemon-inventory/static/media/lucky.f7bd3fd9.png"
+  },
+  {
+    "revision": "985f6e8ce8bd5fdd2a8376eb1b326be1",
+    "url": "/pokemon-inventory/static/media/shiny-bg.985f6e8c.png"
+  },
+  {
+    "revision": "3a2ff09068637a68b44b5b5ca667ed9b",
+    "url": "/pokemon-inventory/static/media/shiny-o.3a2ff090.png"
+  }
+]);
